845414a3dc4479ae70050252d8a0e75c467b5d1b
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-05-06 17:52:06 -0300

    Login page configuration

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+        <input placeholder="e-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+
+        <a href="forgot">Esquecí minha senha</a>
+      </form>
+
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+export default SignIn;
